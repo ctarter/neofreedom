628b4a42363d763d2d7af7d482d7a098e30226e3
root <root@FFADMIN-LAPTOP.localdomain>
2020-09-28 13:11:10 -0400

    mv wp-content

--- wp-content/blogs.dir/4/files/sucuri/sucuri-integrity.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=integrity;
-// created_on=1436485105;
-// updated_on=1436485105;
-exit(0);
-?>
--- wp-content/plugins/gd-topic-polls/forms/dialogs/votes.php
+++
@@ -1,8 +0,0 @@
-<div style="display: none">
-    <div title="<?php _e("Are you sure?", "gd-topic-polls"); ?>" id="gdpol-dialog-votes-delete">
-        <div class="gdpol-inner-content">
-            <p><?php _e("Selected vote will be removed from database.", "gd-topic-polls"); ?></p>
-            <p><?php _e("Are you sure you want to proceed? This operation is not reversable!", "gd-topic-polls"); ?></p>
-        </div>
-    </div>
-</div>
